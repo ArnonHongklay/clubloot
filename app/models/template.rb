5dcb978e41cf092f0a183e2de6d4dbd30b957137
Arnon Hongklay <arnon@hongklay.com>
2016-12-09 20:10:57 +0700

    fixed bug

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,12 +1,12 @@
 class Template
   include Mongoid::Document
-  srore_in collection: 'templates', database: 'clubloot-dev'
+  # srore_in collection: 'templates', database: 'clubloot-dev'
 
   field :name, type: String
   field :program, type: String
   field :number_questions, type: String
   field :number_answers, type: String
-  field :start_time, type: Datetime
-  field :end_time, type: Datetime
+  field :start_time, type: DateTime
+  field :end_time, type: DateTime
   field :active, type: Boolean
 end
