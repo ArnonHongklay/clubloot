b837f851125556eb478de71b150f46c0021353ec
Arnon Hongklay <arnon@hongklay.com>
2016-12-09 19:32:31 +0700

    mongo

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module API
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
