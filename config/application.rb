bc1cac87d27e6c9a46d96f0a8bedbf8c54b8f877
Arnon Hongklay <arnon@hongklay.com>
2017-01-04 17:47:40 +0700

    upgrade rails

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,9 +26,5 @@ module API
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.generators do |g|
-      g.orm :mongoid
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  def add_swagger_route http_method, path, opts = {}
-    full_path = path.gsub(/{(.*?)}/, ':\1')
-    match full_path, to: "#{opts.fetch(:controller_name)}##{opts[:action_name]}", via: http_method
-  end
-
-  # add_swagger_route 'GET', '/v1/estimates/price', controller_name: 'estimates', action_name: 'estimates_price_get'
-  # add_swagger_route 'GET', '/v1/estimates/time',  controller_name: 'estimates', action_name: 'estimates_time_get'
-  # add_swagger_route 'GET', '/v1/products',        controller_name: 'products',  action_name: 'index'
-  # add_swagger_route 'GET', '/v1/history',         controller_name: 'user',      action_name: 'history_get'
-  # add_swagger_route 'GET', '/v1/me',              controller_name: 'user',      action_name: 'me_get'
 end
