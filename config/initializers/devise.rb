b280e6d9a08a0846ee5f04f137173d3e8ad57229
Arnon Hongklay <arnon@hongklay.com>
2017-01-20 17:56:57 +0700

    upgrade rails
    ———
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: ENV['FACEBOOK_CALLBACK']
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: ENV['FACEBOOK_CALLBACK'],
+                  scope: 'email', info_fields: 'email,first_name,last_name,gender,birthday'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # devise_for :users, :controllers => {
-  #   omniauth_callbacks: "users/omniauth_callbacks",
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations'
-  # }
-  devise_for :users
+  devise_for :users, :controllers => {
+    omniauth_callbacks: "users/omniauth_callbacks",
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  get 'users', to: redirect('users/sign_in')
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :login
+  end
 
   # resources :programs do
   #   resources :templates do
