e190975d8fa719a2b750339ff73b8f5f94d66504
Arnon Hongklay <arnon@hongklay.com>
2017-02-01 00:27:56 +0700

    remove
    ———
    
    - How I did it
    I've changed …
    
    - Description for the changelog
    …….

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  resources :contests
-  resources :answers
-  resources :programs
-  resources :questions
-  resources :ledgers
-  resources :templates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, path: '/', contraints: { subdomain: 'api' } do
     devise_for :users
 
-    # resources :programs do
-    #   resources :templates do
-    #   end
-    # end
+    resources :contests
+    resources :answers
+    resources :programs
+    resources :questions
+    resources :ledgers
+    resources :templates
   end
 end
