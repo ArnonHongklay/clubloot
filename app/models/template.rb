5a5d460d468996a2d8996ae61fb8f8ed464429a9
Arnon Hongklay <arnon@hongklay.com>
2017-03-03 02:02:47 +0700

    fixing program

--- app/models/template.rb
+++ app/models/template.rb
@@ -19,12 +19,12 @@ class Template
 
   after_save :check_choice
 
-  scope :upcoming_program, -> { where(:start_time.gte => Time.zone.now, :end_time.lte => Time.zone.now) }
-  scope :live_program, -> { where(:end_time.gte => Time.zone.now) }
-  scope :past_program, -> { where(:end_time.gte => Time.zone.now, :end_time.lte => Time.zone.now) }
+  scope :upcoming_program, -> { where(:start_time.lte => Time.zone.now, :end_time.gte => Time.zone.now) }
+  scope :live_program, -> { where(:end_time.lte => Time.zone.now) }
+  scope :past_program, -> { where(:end_time.lte => Time.zone.now, :end_time.lte => Time.zone.now) }
 
   def self.upcoming_time
-    upcoming = where(:start_time.gte => Time.zone.now, :end_time.lte => Time.zone.now)
+    upcoming = self.upcoming_program
     min = nil
     upcoming.each do |m|
       min = m.end_time if min.nil? or min < m.end_time
