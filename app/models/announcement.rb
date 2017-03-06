9212d18727e76508282564861b35666ff30458a3
Arnon Hongklay <arnon@hongklay.com>
2017-03-06 10:53:30 +0700

    fixing aws

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,19 +4,11 @@ class Announcement
   field :publish, type: Date
   field :description, type: String
 
-  # after_create :push_socket
   after_touch do
-    # CommentRelayJob.perform_later(self)
     self.broadcast
   end
 
   def broadcast
-    # ActionCable.server.broadcast(
-    #   "announcement_#{self.id}",
-    #   publish: self.publish,
-    #   description: self.description
-    # )
-
     AnnouncementChannel.broadcast_to(
       "announcement_#{self.id}",
       action: "announcement",
