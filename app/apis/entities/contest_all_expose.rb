cb4b6ea346020a48276fffe443c297ccad0d36db
Arnon Hongklay <arnon@hongklay.com>
2017-03-06 18:05:39 +0700

    winner

--- app/apis/entities/contest_all_expose.rb
+++ app/apis/entities/contest_all_expose.rb
@@ -18,4 +18,5 @@ class Entities::ContestAllExpose < Grape::Entity
   expose :upcoming_time do |item|
     item.template.end_time
   end
+  expose :leaders
 end
--- app/workers/winner_worker.rb
+++ app/workers/winner_worker.rb
@@ -2,16 +2,20 @@ class WinnerWorker
   include Sidekiq::Worker
 
   def perform(template, contest)
-    questions = template.questions
+    # questions = template.questions
 
-    self.template.contests.each do |contest|
-      contest.quizes.where(question_id: self.id).each do |quiz|
-        if quiz.answer_id == self.is_correct
-          quiz.update(correct: 1)
-        else
-          quiz.update(correct: 0)
-        end
-      end
+    # self.template.contests.each do |contest|
+    #   contest.quizes.where(question_id: self.id).each do |quiz|
+    #     if quiz.answer_id == self.is_correct
+    #       quiz.update(correct: 1)
+    #     else
+    #       quiz.update(correct: 0)
+    #     end
+    #   end
+    # end
+
+    contest.leaders.select{ |l| l.position == 1 }.each do |player|
+      contest.winners << User.find(player.id)
     end
   end
 end
