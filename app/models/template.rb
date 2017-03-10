f1d851849b7a2eeb4f3424bb9d17cc86aa5186e9
Arnon Hongklay <arnon@hongklay.com>
2017-03-10 23:34:22 +0700

    fixing amount when refund use fee only

--- app/models/template.rb
+++ app/models/template.rb
@@ -94,24 +94,28 @@ class Template
       end
     end
 
-    template.contests.where(_state: :cancel).each do |contest|
-      contest.players.each do |player|
-        player.update(coins: player.coins + contest.fee)
 
-        transaction = OpenStruct.new(
-          status: 'complete',
-          format: 'refund',
-          action: 'plus',
-          description: 'Refund contest',
-          from: 'gem',
-          to: 'coins',
-          unit: 'coins',
-          amount: player.coins + contest.fee,
-          tax: 0
-        )
 
-        player.update(coins: player.coins + contest.fee)
-        Ledger.create_transaction(player, transaction)
+    Template.all.each do |template|
+      template.contests.where(_state: :cancel).each do |contest|
+        contest.players.each do |player|
+          player.update(coins: player.coins + contest.fee)
+
+          transaction = OpenStruct.new(
+            status: 'complete',
+            format: 'refund',
+            action: 'plus',
+            description: 'Refund contest',
+            from: 'gem',
+            to: 'coins',
+            unit: 'coins',
+            amount: contest.fee,
+            tax: 0
+          )
+
+          player.update(coins: player.coins + contest.fee)
+          Ledger.create_transaction(player, transaction)
+        end
       end
     end
   end
