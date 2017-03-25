f384e2968a86bbed511a59f36d6fe530dc5da482
Arnon Hongklay <arnon@hongklay.com>
2017-03-25 10:08:32 -0700

    contest socket

--- app/assets/javascripts/channels/announcement.js
+++ app/assets/javascripts/channels/announcement.js
@@ -1,25 +1,25 @@
-// App.announcement = App.cable.subscriptions.create({
-//   channel: "AnnouncementChannel",
-//   token: "T3QPTX3RN95P8N9MG85AFXUKCRK6MHQJ"
-// }, {
-//   connected: function() {
-//     this.show('T3QPTX3RN95P8N9MG85AFXUKCRK6MHQJ')
-//   },
-//   disconnected: function() {
+App.announcement = App.cable.subscriptions.create({
+  channel: "AnnouncementChannel",
+  token: "T3QPTX3RN95P8N9MG85AFXUKCRK6MHQJ"
+}, {
+  connected: function() {
+    this.show('T3QPTX3RN95P8N9MG85AFXUKCRK6MHQJ')
+  },
+  disconnected: function() {
 
-//   },
-//   received: function(data) {
-//     console.log(data);
-//   },
-//   show: function(data) {
-//     return this.perform('show', {
-//       token: data
-//     });
-//   },
-//   destroy: function(token, announcement) {
-//     return this.perform('destroy', {
-//       token: token,
-//       announcement: announcement
-//     });
-//   }
-// });
+  },
+  received: function(data) {
+    console.log(data);
+  },
+  show: function(data) {
+    return this.perform('show', {
+      token: data
+    });
+  },
+  destroy: function(token, announcement) {
+    return this.perform('destroy', {
+      token: token,
+      announcement: announcement
+    });
+  }
+});
--- app/assets/javascripts/channels/contest.js
+++ app/assets/javascripts/channels/contest.js
@@ -1,7 +1,15 @@
-// App.contest = App.cable.subscriptions.create("ContestChannel", {
-//   connected: function() {},
-//   disconnected: function() {},
-//   received: function(data) {
-//     console.log(data);
-//   }
-// });
+App.contest = App.cable.subscriptions.create("ContestChannel", {
+  connected: function() {},
+  disconnected: function() {},
+  received: function(data) {
+    console.log(data);
+  },
+  // show: function(message) {
+  //   return this.perform(data);
+  // }
+  speak: function(message) {
+    return this.perform('speak', {
+      message: message
+    });
+  }
+});
