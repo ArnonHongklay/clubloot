4b1969950c44a7ebca5ee4512c7ecd16d78b8fb3
Arnon Hongklay <arnon@hongklay.com>
2017-03-25 10:36:38 -0700

    user api all contest

--- app/apis/v1/users_api.rb
+++ app/apis/v1/users_api.rb
@@ -30,19 +30,19 @@ module V1
       resource :contests do
         params do
           requires :token, type: String, default: nil, desc: 'User Token'
-          optional :state, type: String, default: "upcoming", desc: 'upcoming, live, past, winners'
+          optional :state, type: String, default: "upcoming", desc: 'all, upcoming, live, past, winners'
         end
         get "/" do
           begin
             if user = User.find_by(token: params[:token])
               if params[:state].eql?('winners')
                 contests = user.winners.order(updated_at: :desc)
+              elsif params[:state].eql?('past')
+                contests = user.contests.where(_state: { '$in': [:end, :cancel]}).order(updated_at: :desc)
+              elsif params[:state].eql?('all')
+                contests = user.contests.order(updated_at: :desc)
               else
-                if params[:state].eql?('past')
-                  contests = user.contests.where(_state: { '$in': [:end, :cancel]}).order(updated_at: :desc)
-                else
-                  contests = user.contests.where(_state: params[:state]).order(updated_at: :desc)
-                end
+                contests = user.contests.where(_state: params[:state]).order(updated_at: :desc)
               end
               present :status, :success
               present :data, contests, with: Entities::ContestAllExpose
