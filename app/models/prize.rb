2866b4f76d0aa8782e04262fc9f99749421e935e
Arnon Hongklay <arnon@hongklay.com>
2017-03-27 09:56:24 +0700

    fixing prize

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -11,7 +11,7 @@ class Prize
   field :active,      type: Boolean
   field :count,       type: Integer
 
-  has_and_belongs_to_many :users, class_name: 'Users', inverse_of: :prizes
+  has_and_belongs_to_many :users, class_name: 'User', inverse_of: :prizes
 
   has_mongoid_attached_file :attachment
   validates_attachment :attachment, content_type: { content_type: /\Aimage\/.*\Z/ }
