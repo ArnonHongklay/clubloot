e94b8218aa5bed4e8b55cf66b3e45c84f9026944
Arnon Hongklay <arnon@hongklay.com>
2017-03-27 11:16:38 +0700

    dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,18 +6,27 @@ class DashboardController < ApplicationController
   def index
     @richs = User.order(coins: :desc).limit(10)
     @contests = Contest.upcoming
+    if params[:all].nil?
+      if params[:start].present? and params[:end].present?
+        players = User.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        contests = Contest.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
 
-    if params[:start].present? and params[:end].present?
-      players = User.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
-      contests = Contest.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        conomy = ConomyLog.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        prize = Ledger.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        tax = Tax.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
 
-      conomy = ConomyLog.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
-      prize = Ledger.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
-      tax = Tax.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        sign_in_range = SigninLog.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        sign_in_all   = sign_in_range.group_by(&:group_by_criteria).map {|k,v| v.length }
+        sign_in       = sign_in_all.inject{ |sum, el| sum + el }.to_f / sign_in_all.size
+      else
+        players = User.where(:created_at.gte => Time.zone.now.beginning_of_day)
+        contests = Contest.where(:created_at.gte => Time.zone.now.beginning_of_day)
 
-      sign_in_range = SigninLog.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
-      sign_in_all   = sign_in_range.group_by(&:group_by_criteria).map {|k,v| v.length }
-      sign_in       = sign_in_all.inject{ |sum, el| sum + el }.to_f / sign_in_all.size
+        conomy = ConomyLog.where(:created_at.gte => Time.zone.now.beginning_of_day)
+        prize = Ledger.where(:created_at.gte => Time.zone.now.beginning_of_day)
+        tax = Tax.where(:created_at.gte => Time.zone.now.beginning_of_day)
+        sign_in = SigninLog.where(:created_at.gte => Time.zone.now.beginning_of_day).count
+      end
     else
       players = User.all
       contests = Contest.all
@@ -25,7 +34,9 @@ class DashboardController < ApplicationController
       conomy = ConomyLog.all
       prize = Ledger.all
       tax = Tax.all
-      sign_in = SigninLog.where(:created_at.gte => Time.zone.now.beginning_of_day).count
+      sign_in_range = SigninLog.all
+      sign_in_all   = sign_in_range.group_by(&:group_by_criteria).map {|k,v| v.length }
+      sign_in       = sign_in_all.inject{ |sum, el| sum + el }.to_f / sign_in_all.size
     end
     @total_players = players.count
     @total_contests = contests.count
