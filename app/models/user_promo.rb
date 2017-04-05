b1569706bfef6b7c7914d96f8ee02a6d4aca9083
Arnon Hongklay <arnon@hongklay.com>
2017-04-05 20:18:08 +0700

    user promo

--- app/models/user_promo.rb
+++
@@ -1,8 +0,0 @@
-class UserPromo
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  include Mongoid::Attributes::Dynamic
-
-  belongs_to :user
-  belongs_to :promo
-end
--- spec/factories/user_promos.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :user_promo do
-    
-  end
-end
