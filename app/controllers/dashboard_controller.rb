4d5d3edd6b030d3dcfe03ae62de39de85c0351e6
Arnon Hongklay <arnon@hongklay.com>
2017-04-07 19:27:25 +0700

    fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,7 +14,7 @@ class DashboardController < ApplicationController
         economy = Economy.where(:logged_at.gte => Time.zone.parse(params[:start]), :logged_at.lte => Time.zone.parse(params[:end]))
         prize = Ledger.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
 
-        sign_in_range = SigninLog.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
+        sign_in_range = ApiKey.where(:created_at.gte => Time.zone.parse(params[:start]), :created_at.lte => Time.zone.parse(params[:end]))
         sign_in_all   = sign_in_range.group_by(&:group_by_criteria).map {|k,v| v.length }
         sign_in       = sign_in_all.inject{ |sum, el| sum + el }.to_f / sign_in_all.size
       else
@@ -23,7 +23,7 @@ class DashboardController < ApplicationController
 
         economy = Economy.where(:logged_at.gte => Time.zone.now.beginning_of_day)
         prize = Ledger.where(:created_at.gte => Time.zone.now.beginning_of_day)
-        sign_in = SigninLog.where(:created_at.gte => Time.zone.now.beginning_of_day).count
+        sign_in = ApiKey.where(:created_at.gte => Time.zone.now.beginning_of_day).count
       end
     else
       players = User.all
@@ -32,7 +32,7 @@ class DashboardController < ApplicationController
       economy = Economy.all
       prize = Ledger.all
 
-      sign_in_range = SigninLog.all
+      sign_in_range = ApiKey.all
       sign_in_all   = sign_in_range.group_by(&:group_by_criteria).map {|k,v| v.length }
       sign_in       = sign_in_all.inject{ |sum, el| sum + el }.to_f / sign_in_all.size
     end
--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,16 +2,22 @@ class ApiKey
   include Mongoid::Document
   field :access_token, type: String
   field :expires_at, type: Time
-  field :user_id, type: Integer
+  # field :user_id, type: String
   field :active, type: Mongoid::Boolean
 
   # attr_accessible :access_token, :expires_at, :user_id, :active, :application
+  belongs_to :user
+
   before_create :generate_access_token
   before_create :set_expiration
-  belongs_to :user
+  after_create :daily_loot
 
   def expired?
     Time.zone.now >= self.expires_at
+  end
+
+  def group_by_criteria
+    created_at.to_date.to_s(:db)
   end
 
   private
@@ -23,5 +29,12 @@ class ApiKey
 
     def set_expiration
       self.expires_at = Time.zone.now + 30
+    end
+
+    def set_signin_log
+      if where(:created_at.gte => Time.zone.now.beginning_of_day).count <= 0
+        self.user.update(coins: self.user.coins + 2000)
+        # Ledger.
+      end
     end
 end
