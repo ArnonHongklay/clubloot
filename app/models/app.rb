83510ea3d4269caaf75b7ed35525fcd0aaca191e
Arnon Hongklay <arnon@hongklay.com>
2017-04-09 05:09:36 +0700

    ?

--- app/models/app.rb
+++ app/models/app.rb
@@ -94,14 +94,14 @@ class GemConvert
     case type
     when 'sapphire'
       raise "coins or ruby less" if user.coins < gemc.ruby[:fee].to_i or user.rubies < gemc.ruby[:rate].to_i
-      user.coins    = user.coins - gemc.ruby[:fee].to_i
-      user.rubies   = user.rubies - gemc.ruby[:rate].to_i
-      user.diamonds = user.diamonds + 1
+      user.coins     = user.coins - gemc.ruby[:fee].to_i
+      user.rubies    = user.rubies - gemc.ruby[:rate].to_i
+      user.sapphires = user.sapphires + 1
     when 'emerald'
       raise "coins or sapphire less" if user.coins < gemc.sapphire[:fee].to_i or user.sapphires < gemc.sapphire[:rate].to_i
-      user.coins    = user.coins - gemc.sapphire[:fee].to_i
+      user.coins     = user.coins - gemc.sapphire[:fee].to_i
       user.sapphires = user.sapphires - gemc.sapphire[:rate].to_i
-      user.diamonds = user.diamonds + 1
+      user.emeralds  = user.emeralds + 1
     when 'diamond'
       raise "coins or emerald less" if user.coins < gemc.emerald[:fee].to_i or user.emeralds < gemc.emerald[:rate].to_i
       user.coins    = user.coins - gemc.emerald[:fee].to_i
