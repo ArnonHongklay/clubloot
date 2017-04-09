d9b672a3088ae4ddbf0729e5225f4ee903544919
Arnon Hongklay <arnon@hongklay.com>
2017-04-09 19:40:52 +0700

    ?

--- app/apis/v1/contests_api.rb
+++ app/apis/v1/contests_api.rb
@@ -191,7 +191,7 @@ module V1
       end
       get '/templates' do
         begin
-          if templates = Template.active.where(program: params[:program_id])
+          if templates = Template.active.where(program: params[:program_id]).where(:end_time.gte => Time.zone.now)
             present :status, :success
           else
             present :status, :failure
