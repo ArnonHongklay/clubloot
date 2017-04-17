541d787b83c629c3707c789ecfb79b6ef773c246
Arnon Hongklay <arnon@hongklay.com>
2017-04-17 20:41:56 +0700

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,31 +1,31 @@
-# Be sure to restart your server when you modify this file.
+# # Be sure to restart your server when you modify this file.
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+# # Avoid CORS issues when API is called from the frontend app.
+# # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-# Read more: https://github.com/cyu/rack-cors
+# # Read more: https://github.com/cyu/rack-cors
+
+# # Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# #   allow do
+# #     origins '*'
+
+# #     resource '*',
+# #       headers: :any,
+# #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+# #   end
+# # end
 
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #   allow do
+#     # origins '*clubloot.(com|local)'
 #     origins '*'
-
 #     resource '*',
 #       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#       medthods: [:get, :post, :put, :delete]
 #   end
+
+#   # allow do
+#   #  origins '*'
+#   #  resource '/public/*', headers: :any, medthods: :get
+#   # end
 # end
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    # origins '*clubloot.(com|local)'
-    origins '*'
-    resource '*',
-      headers: :any,
-      medthods: [:get, :post, :put, :delete]
-  end
-
-  # allow do
-  #  origins '*'
-  #  resource '/public/*', headers: :any, medthods: :get
-  # end
-end
