b2f152a830e73b872e5c00c2b088b66b4e4f541d
Arnon Hongklay <arnon@hongklay.com>
2017-04-17 20:42:38 +0700

    test

--- config/application.rb
+++ config/application.rb
@@ -52,5 +52,11 @@ module Clubloot
       }
     }
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
