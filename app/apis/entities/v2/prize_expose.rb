ad80e16dde707e58e0dccec57caa510f11981a06
Arnon Hongklay <arnon@hongklay.com>
2017-04-27 02:16:30 +0700

    ok

--- app/apis/entities/v2/prize_expose.rb
+++ app/apis/entities/v2/prize_expose.rb
@@ -1,3 +1,9 @@
 class Entities::V2::PrizeExpose < Grape::Entity
+  expose :name
+  expose :price
+  expose :quantity
+  expose :description
+  expose :active
+  expose :count
   expose :prize, with: Entities::V2::PrizesExpose
 end
