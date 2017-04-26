0a818c58801c6f3dc68a4d7cd026b20463cf265e
Arnon Hongklay <arnon@hongklay.com>
2017-04-26 20:43:16 +0700

    try fix socket

--- config/initializers/web_socket.rb
+++ config/initializers/web_socket.rb
@@ -1,3 +1,3 @@
 Rails.application.config.action_cable.url = "ws://api.#{App.root_domain}/cable"
 Rails.application.config.action_cable.allowed_request_origins = [ App.domain, App.domain('admin'), App.domain('api'), 'alpha.clubloot.com', 'staging.clubloot.com' ]
-Rails.application.config.action_cable.disable_request_forgery_protection = true
+Rails.application.config.action_cable.disable_request_forgery_protection = false
