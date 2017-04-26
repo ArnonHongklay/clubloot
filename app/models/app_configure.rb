6155da0b17540aed798b6afa57afdb0e5b004c23
Arnon Hongklay <arnon@hongklay.com>
2017-04-26 11:42:57 +0700

    exchange

--- app/models/app_configure.rb
+++ app/models/app_configure.rb
@@ -19,18 +19,65 @@ class AppConfigure
       user.coins     = user.coins - gemc.sapphire[:fee].to_i
       user.rubies    = user.rubies - gemc.ruby[:rate].to_i
       user.sapphires = user.sapphires + 1
+
+      from = 'rubies'
+      to = 'sapphires'
     when 'emerald'
       raise "coins or sapphire less" if user.coins < gemc.sapphire[:fee].to_i or user.sapphires < gemc.sapphire[:rate].to_i
       user.coins     = user.coins - gemc.emerald[:fee].to_i
       user.sapphires = user.sapphires - gemc.sapphire[:rate].to_i
       user.emeralds  = user.emeralds + 1
+
+      from = 'sapphires'
+      to = 'emeralds'
     when 'diamond'
       raise "coins or emerald less" if user.coins < gemc.emerald[:fee].to_i or user.emeralds < gemc.emerald[:rate].to_i
       user.coins     = user.coins - gemc.diamond[:fee].to_i
       user.emeralds  = user.emeralds - gemc.emerald[:rate].to_i
       user.diamonds  = user.diamonds + 1
+
+      from = 'emeralds'
+      to = 'diamonds'
     end
 
     user.save!
+
+
+    transactions = []
+    transactions << OpenStruct.new(
+      status: 'complete',
+      format: 'Convert Gem',
+      action: 'minus',
+      description: 'Convert Gem',
+      from: 'coins',
+      to: to,
+      unit: to,
+      amount: 0,
+      tax: 0
+    )
+    transactions << OpenStruct.new(
+      status: 'complete',
+      format: 'Convert Gem',
+      action: 'minus',
+      description: 'Convert Gem',
+      from: from,
+      to: to,
+      unit: from,
+      amount: 0,
+      tax: 0
+    )
+    transactions << OpenStruct.new(
+      status: 'complete',
+      format: 'Convert Gem',
+      action: 'plus',
+      description: 'Convert Gem',
+      from: to,
+      to: "#{from} & coins",
+      unit: to,
+      amount: 0,
+      tax: 0
+    )
+
+    Ledger.create_transactions(user, transactions)
   end
 end
