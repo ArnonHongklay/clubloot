6ece4ef2ee75542e78f457558ab105741716a127
Arnon Hongklay <arnon@hongklay.com>
2017-05-11 21:42:17 +0700

    save 5.1

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.1.0'
 
 platform :ruby do
   gem 'pg', '~> 0.18'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,9 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( admin.js )
-# Rails.application.config.assets.precompile += %w( templates/*.js templates/*.css )
-
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
